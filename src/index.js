a14263ed257fdf416afcf9a86cf31738e3ce4dcb
Raximjon Komiljonov <komiljonovrahimjon@gmail.com>
2019-09-26 00:53:51 +0500

    init: solving task Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let code = str.split(''),
+        cloneInArr = false,
+        stack = [],
+        openTag = bracketsConfig.map((cur) => cur[0]),
+        closeTag = bracketsConfig.map((cur) => cur[1]);
+
+    code.map((cur, index) => {
+        if (openTag.includes(cur)) {
+
+            if (closeTag.includes(cur)) {
+                cloneInArr = true;
+            }
+
+            if (cloneInArr && closeTag.indexOf(cur) === openTag.indexOf(stack[stack.length-1])) {
+                cloneInArr = false;
+                stack.pop();
+            } else {
+                stack.push(cur);
+            }
+
+        } else if (closeTag.indexOf(cur) === openTag.indexOf(stack[stack.length-1])){
+
+            stack.pop();
+
+        }
+    });
+
+    return !stack.length > 0;
 }
